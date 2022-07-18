604a784f4033e5da8269b2d6c028a7dbfad280bb
25081997pc <chavanpooja2508@gmail.com>
2022-07-18 21:01:34 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparision_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Program ");
+        }
+    }
+}
