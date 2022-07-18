608eab25586f3eb14b9bcb8bf9c947ade5ea2843
25081997pc <chavanpooja2508@gmail.com>
2022-07-18 21:14:47 +0530

    Adding object

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Program ");
+            MeasureLineLength measureLineLength = new MeasureLineLength(0);
+            measureLineLength.MeasuretheLength();
         }
     }
 }
